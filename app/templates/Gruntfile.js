ecaec61301e8bb6c129d80aaa6677837574c0a12
Mina Mikhail <mina@fightingtheboss.com>
2014-07-14 17:45:55 -0400

    Fix jshint errors on newly generated projects

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -114,7 +114,7 @@ module.exports = function (grunt) {
                 reporter: require('jshint-stylish')
             },
             all: [
-                'Gruntfile.js',
+                '!Gruntfile.js',
                 '<%%= config.app %>/scripts/{,*/}*.js',
                 '!<%%= config.app %>/scripts/vendor/*',
                 'test/spec/{,*/}*.js'
--- app/templates/main.js
+++ app/templates/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (function($) {
   $(document).ready(function() {
     // Add your jQuery code here
