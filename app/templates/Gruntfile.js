851d9c7a508d2de2f2ceb141dda34b6c448002db
Mina Mikhail <mina@fightingtheboss.com>
2015-01-16 13:41:35 -0500

    Fix Gruntfile so it copies all images on build

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -193,7 +193,7 @@ module.exports = function (grunt) {
                     src: [
                         '*.{ico,png,txt}',
                         '.htaccess',
-                        'images/{,*/}*.webp',
+                        'images/{,*/}*.*',
                         '{,*/}*.html',
                         'styles/fonts/{,*/}*.*',
                         'images',
