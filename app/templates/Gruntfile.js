f1d71e9b5e6ac758f27ca1ed45c8211183ab23e0
Mina Mikhail <mina@fightingtheboss.com>
2015-06-10 16:32:55 -0400

    Fix for missing comma in copy task

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -195,7 +195,7 @@ module.exports = function (grunt) {
                         '.htaccess',
                         'images/**',
                         'styles/fonts/**',
-                        'fonts/**'
+                        'fonts/**',
                         'README.md'
                     ]
                 },{
