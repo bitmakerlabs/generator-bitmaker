017a10a04861efc5f62410f338dd595944be5b69
Mina Mikhail <mina@fightingtheboss.com>
2015-06-22 16:23:48 -0400

    Update Gruntfile and gitignore for Ruby sass

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -126,7 +126,7 @@ module.exports = function (grunt) {
         // Compiles Sass to CSS and generates necessary files if requested
         sass: {
             options: {
-                includePaths: [
+                loadPath: [
                     'bower_components'
                 ]
             },
